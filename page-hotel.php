64048e3c6b95f40e2d4b0105dd5c57118d666bca
Seth <seth_wieder@condenast.com>
2015-07-30 00:03:15 -0400

    adding hotel page

---
+++ page-hotel.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Template Name: Hotel Page
+ *
+ * @package parque
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		
+		<div class="page-topimg page-topimg--dining">
+			<div class="toptabs-contain">
+				<div class="toptabs">
+					<div class="toptab font-lecker active"><a href="#">Hotel</a></div>
+				</div>
+			</div>
+		</div>
+
+		<main id="main" class="site-main" role="main">
+
+		<div class="border-wave"></div>
+
+		<div class="casa-row clear casa-row--nopad" id="hotel-intro">
+		    <div class="casa-row clear">
+				<div class="casa-col--bg casa-col--right casa-col--30p"></div>
+				<div class="casa-col--text  casa-col--60p blue-on-white padding--top-0">
+					<p class="parque-text-color--bluedark parque-body--sm dining-body">
+						From the moment you walk through the door, you will be struck by an incredible, breathtaking view that frames the crystal waters of Cancun Bay. Casa de los Suenos is a unique and intimate property that radiates tranquility, peace, and a sense of "home".
+						<br/><br/>
+						Authentic and genuine, Casa de los Sueños complements the vibe of Isla Mujeres without sacrificing luxury. It's a perfect choice whether you're planning a family reunion, a destination wedding in Mexico, or an exclusive and relaxing retreat.
+						<br/><br/>
+						<a href="http://www.casasuenos.com" target="_blank" class="parque-text-color--bluedark parque-body--bold font-underline">Please visit the Casa de los Suenos website to learn more.</a>
+					</p>
+				</div>
+			</div>
+		</div>
+
+		<div class="border-wave"></div>
+
+		<div class="casa-row clear" id="hotel-link">
+		    <div class="casa-col--bg casa-col--60p casa-col--right"></div>
+		    <div class="casa-col--text casa-col--40p casa-col--extrapad casa-col--orange-dark casa-col--pattern-circle clear">
+		        <a href="http://www.casasuenos.com" target="_blank" class="font-no-underline">
+		        <h1 class="parque-XL text-center parque-text-color--white"><span class="font-lecker">Visit</span><br><span class="font-uppercase">The<br/>Hotel<br/>Site</span></h1>
+		        </a>
+		    </div>
+		</div>
+			
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php get_footer(); ?>
