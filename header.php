a4380dd00ad6312a500390bdb7180faa7322ef4b
Seth <seth_wieder@condenast.com>
2015-07-30 23:37:42 -0400

    fav url wasnt working. set to abs path of staging server

--- header.php
+++ header.php
@@ -14,27 +14,27 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-<link rel="apple-touch-icon" sizes="57x57" href="/favicons/apple-touch-icon-57x57.png">
-<link rel="apple-touch-icon" sizes="60x60" href="/favicons/apple-touch-icon-60x60.png">
-<link rel="apple-touch-icon" sizes="72x72" href="/favicons/apple-touch-icon-72x72.png">
-<link rel="apple-touch-icon" sizes="76x76" href="/favicons/apple-touch-icon-76x76.png">
-<link rel="apple-touch-icon" sizes="114x114" href="/favicons/apple-touch-icon-114x114.png">
-<link rel="apple-touch-icon" sizes="120x120" href="/favicons/apple-touch-icon-120x120.png">
-<link rel="apple-touch-icon" sizes="144x144" href="/favicons/apple-touch-icon-144x144.png">
-<link rel="apple-touch-icon" sizes="152x152" href="/favicons/apple-touch-icon-152x152.png">
-<link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon-180x180.png">
-<link rel="icon" type="image/png" href="/favicons/favicon-32x32.png" sizes="32x32">
-<link rel="icon" type="image/png" href="/favicons/favicon-194x194.png" sizes="194x194">
-<link rel="icon" type="image/png" href="/favicons/favicon-96x96.png" sizes="96x96">
-<link rel="icon" type="image/png" href="/favicons/android-chrome-192x192.png" sizes="192x192">
-<link rel="icon" type="image/png" href="/favicons/favicon-16x16.png" sizes="16x16">
-<link rel="manifest" href="/favicons/manifest.json">
-<link rel="shortcut icon" href="/favicons/favicon.ico">
+<link rel="apple-touch-icon" sizes="57x57" href="http://parque.staging.sethwieder.com/wp-content/themes/parquesuenos/favicons/apple-touch-icon-57x57.png">
+<link rel="apple-touch-icon" sizes="60x60" href="http://parque.staging.sethwieder.com/wp-content/themes/parquesuenos/favicons/apple-touch-icon-60x60.png">
+<link rel="apple-touch-icon" sizes="72x72" href="http://parque.staging.sethwieder.com/wp-content/themes/parquesuenos/favicons/apple-touch-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="76x76" href="http://parque.staging.sethwieder.com/wp-content/themes/parquesuenos/favicons/apple-touch-icon-76x76.png">
+<link rel="apple-touch-icon" sizes="114x114" href="http://parque.staging.sethwieder.com/wp-content/themes/parquesuenos/favicons/apple-touch-icon-114x114.png">
+<link rel="apple-touch-icon" sizes="120x120" href="http://parque.staging.sethwieder.com/wp-content/themes/parquesuenos/favicons/apple-touch-icon-120x120.png">
+<link rel="apple-touch-icon" sizes="144x144" href="http://parque.staging.sethwieder.com/wp-content/themes/parquesuenos/favicons/apple-touch-icon-144x144.png">
+<link rel="apple-touch-icon" sizes="152x152" href="http://parque.staging.sethwieder.com/wp-content/themes/parquesuenos/favicons/apple-touch-icon-152x152.png">
+<link rel="apple-touch-icon" sizes="180x180" href="http://parque.staging.sethwieder.com/wp-content/themes/parquesuenos/favicons/apple-touch-icon-180x180.png">
+<link rel="icon" type="image/png" href="http://parque.staging.sethwieder.com/wp-content/themes/parquesuenos/favicons/favicon-32x32.png" sizes="32x32">
+<link rel="icon" type="image/png" href="http://parque.staging.sethwieder.com/wp-content/themes/parquesuenos/favicons/favicon-194x194.png" sizes="194x194">
+<link rel="icon" type="image/png" href="http://parque.staging.sethwieder.com/wp-content/themes/parquesuenos/favicons/favicon-96x96.png" sizes="96x96">
+<link rel="icon" type="image/png" href="http://parque.staging.sethwieder.com/wp-content/themes/parquesuenos/favicons/android-chrome-192x192.png" sizes="192x192">
+<link rel="icon" type="image/png" href="http://parque.staging.sethwieder.com/wp-content/themes/parquesuenos/favicons/favicon-16x16.png" sizes="16x16">
+<link rel="manifest" href="http://parque.staging.sethwieder.com/wp-content/themes/parquesuenos/favicons/manifest.json">
+<link rel="shortcut icon" href="http://parque.staging.sethwieder.com/wp-content/themes/parquesuenos/favicons/favicon.ico">
 <meta name="apple-mobile-web-app-title" content="Parque de los Suenos">
 <meta name="application-name" content="Parque de los Suenos">
 <meta name="msapplication-TileColor" content="#2d89ef">
-<meta name="msapplication-TileImage" content="/favicons/mstile-144x144.png">
-<meta name="msapplication-config" content="/favicons/browserconfig.xml">
+<meta name="msapplication-TileImage" content="http://parque.staging.sethwieder.com/wp-content/themes/parquesuenos/favicons/mstile-144x144.png">
+<meta name="msapplication-config" content="http://parque.staging.sethwieder.com/wp-content/themes/parquesuenos/favicons/browserconfig.xml">
 <meta name="theme-color" content="#ffffff">
 <script>
 	/* Test for flexbox supprt. If found, add class to DOM */
